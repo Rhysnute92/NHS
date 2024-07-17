e3d1409e0419f2ca7b74f011bf9469fc48cb1564
c1810126 <HuntR9@cardiff.ac.uk>
2024-07-17 19:36:22 +0100

    Mobile view for landing, guest landing and basic info sections. Desktop View for future development for guest page and info.

---
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/GuestController.java
@@ -0,0 +1,37 @@
+package uk.ac.cf.spring.nhs;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import uk.ac.cf.spring.nhs.common.util.DeviceDetector;
+
+@Controller
+public class GuestController {
+    @GetMapping("/guest")
+    public String showGuestLandingPage(HttpServletRequest request) {
+        if (DeviceDetector.isMobile(request)) {
+            return "/mobile/guestLanding";
+        } else {
+            return "/desktop/guestlanding";
+        }
+    }
+
+    @GetMapping("/landing")
+    public String showLandingPage(HttpServletRequest request) {
+        if (DeviceDetector.isMobile(request)) {
+            return "/mobile/landing";
+        } else {
+            return "/desktop/guestLanding";
+        }
+    }
+
+    @GetMapping("/guest/appointment")
+    public String showGuestAppointmentPage() {return "/mobile/guestAppointment";}
+
+    @GetMapping("/guest/services")
+    public String showGuestServicesPage() {return "/mobile/guestServices";}
+
+    @GetMapping("/guest/find")
+    public String showGuestFindUsPage() {return "/mobile/guestFindUs";}
+
+}
+
