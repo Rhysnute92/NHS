5285d43bc0aa3eebba33644e901eb42a67c0545d
c1950120 <murinsk@cardiff.ac.uk>
2024-07-17 17:03:41 +0100

    added tests

---
+++ nhs/src/test/java/uk/ac/cf/spring/nhs/Education/Controller/EducationControllerTest.java
@@ -0,0 +1,42 @@
+package uk.ac.cf.spring.nhs.Education.Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EducationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void mobileInfopageTest() throws Exception {
+        mockMvc.perform(get("/information")
+                .header("User-Agent",
+                        "Mozilla/5.0 (Linux; Android 10; Pixel 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4076.0 Mobile Safari/537.36"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("mobile/information"));
+    }
+
+    @Test
+    void desktopInfopageTest() throws Exception {
+        mockMvc.perform(get("/information")
+                .header("User-Agent",
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("desktop/information"));
+            }
+
+    @Test
+    void infopageTest() throws Exception {
+        mockMvc.perform(get("/information"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("desktop/information"));
+    }
+}
