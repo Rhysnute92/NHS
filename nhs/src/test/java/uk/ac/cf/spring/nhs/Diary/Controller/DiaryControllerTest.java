ee882a548a2a2fcb6e5531f844d18211b0752c7a
Rowan <rowan.sbur@gmail.com>
2024-07-19 12:35:58 +0100

    Update diary controller tests

--- nhs/src/test/java/uk/ac/cf/spring/nhs/Diary/Controller/DiaryControllerTest.java
+++ nhs/src/test/java/uk/ac/cf/spring/nhs/Diary/Controller/DiaryControllerTest.java
@@ -8,16 +8,25 @@ import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 import uk.ac.cf.spring.nhs.Common.util.DeviceDetector;
+import uk.ac.cf.spring.nhs.Diary.DiaryEntry;
+import uk.ac.cf.spring.nhs.Diary.Service.DiaryService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
 public class DiaryControllerTest {
 
-
     @InjectMocks
     private DiaryController diaryController;
+
+    @Mock
+    private DiaryService diaryService;
 
     private MockHttpServletRequest request;
 
@@ -40,15 +49,31 @@ public class DiaryControllerTest {
 
     @Test
     public void returnsMobileViewForMobileDevices() {
+        // Mock DeviceDetector to return true for isMobile
         mockedStatic.when(() -> DeviceDetector.isMobile(request)).thenReturn(true);
-        String viewName = diaryController.diary(request);
-        assertEquals("mobile/diary", viewName);
+
+        // Mock diaryService to return an empty list
+        List<DiaryEntry> mockDiaryEntries = new ArrayList<>();
+        when(diaryService.getDiaryEntries()).thenReturn(mockDiaryEntries);
+
+        // Call the method and check the view name
+        ModelAndView modelAndView = diaryController.diary(request);
+        assertEquals("mobile/diary", modelAndView.getViewName());
+        assertEquals(mockDiaryEntries, modelAndView.getModel().get("diaryEntries"));
     }
 
     @Test
     public void returnsDesktopViewForNonMobileDevices() {
+        // Mock DeviceDetector to return false for isMobile
         mockedStatic.when(() -> DeviceDetector.isMobile(request)).thenReturn(false);
-        String viewName = diaryController.diary(request);
-        assertEquals("desktop/diary", viewName);
+
+        // Mock diaryService to return an empty list
+        List<DiaryEntry> mockDiaryEntries = new ArrayList<>();
+        when(diaryService.getDiaryEntries()).thenReturn(mockDiaryEntries);
+
+        // Call the method and check the view name
+        ModelAndView modelAndView = diaryController.diary(request);
+        assertEquals("desktop/diary", modelAndView.getViewName());
+        assertEquals(mockDiaryEntries, modelAndView.getModel().get("diaryEntries"));
     }
 }
