7950319a8d856c19a5e124339e66ec59919401a9
Rowan Burnett <burnettrs@cardiff.ac.uk>
2024-07-20 16:46:37 +0100

    Use new nav on all pages

--- nhs/src/main/java/uk/ac/cf/spring/nhs/Diary/Controller/DiaryController.java
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/Diary/Controller/DiaryController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import uk.ac.cf.spring.nhs.Common.util.DeviceDetector;
 import uk.ac.cf.spring.nhs.Diary.Entity.DiaryEntry;
@@ -12,12 +13,13 @@ import uk.ac.cf.spring.nhs.Diary.Service.DiaryService;
 import java.util.List;
 
 @Controller
+@RequestMapping("/diary")
 public class DiaryController {
 
     @Autowired
     DiaryService diaryService;
 
-    @GetMapping("/diary")
+    @GetMapping("")
     public ModelAndView diary(HttpServletRequest request) {
         ModelAndView modelAndView = new ModelAndView();
         List<DiaryEntry> diaryEntries = diaryService.getDiaryEntries();
@@ -25,11 +27,19 @@ public class DiaryController {
         modelAndView.addObject("diaryEntries", diaryEntries);
 
         if (DeviceDetector.isMobile(request)) {
-            modelAndView.setViewName("mobile/diary");
+            modelAndView.addObject("isMobile", true);
         } else {
-            modelAndView.setViewName("desktop/diary");
+            modelAndView.addObject("isMobile", false);
         }
 
+        modelAndView.setViewName("diary");
         return modelAndView;
     }
+
+//    @GetMapping("/add") {
+//    public ModelAndView addDiaryEntry(HttpServletRequest request) {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("desktop/addDiaryEntry");
+//        return modelAndView;
+//    }
 }
