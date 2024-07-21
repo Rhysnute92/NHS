4b0b74af1248830fb508cb1b5e354dd97dea8779
c1810126 <HuntR9@cardiff.ac.uk>
2024-07-21 23:38:11 +0100

    Management placeholder pages with controller and basic testing

---
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/Management/ManagementController.java
@@ -0,0 +1,18 @@
+package uk.ac.cf.spring.nhs.Management;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import uk.ac.cf.spring.nhs.Common.util.DeviceDetector;
+
+@Controller
+public class ManagementController {
+    @GetMapping("/management")
+    public String showManagementPage(HttpServletRequest request) {
+        if (DeviceDetector.isMobile(request)) {
+            return "mobile/management";
+        } else {
+            return "desktop/management";
+        }
+    }
+}
