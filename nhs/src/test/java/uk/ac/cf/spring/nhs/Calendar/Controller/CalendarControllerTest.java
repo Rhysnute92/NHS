a7848b6c08f2a82594fe41ac53393a4f53518f4a
Rhys Nute <nuterd@cardiff.ac.uk>
2024-07-21 21:22:16 +0100

    updated

--- nhs/src/test/java/uk/ac/cf/spring/nhs/Calendar/Controller/CalendarControllerTest.java
+++ nhs/src/test/java/uk/ac/cf/spring/nhs/Calendar/Controller/CalendarControllerTest.java
@@ -26,7 +26,7 @@ public class CalendarControllerTest {
 
     @Test
     public void testMobileCalendar() throws Exception {
-        mockMvc.perform(get("/Calendar")
+        mockMvc.perform(get("/calendar")
                         .header("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("mobile/calendar"));
@@ -34,7 +34,7 @@ public class CalendarControllerTest {
 
     @Test
     public void testDesktopCalendar() throws Exception {
-        mockMvc.perform(get("/Calendar")
+        mockMvc.perform(get("/calendar")
                         .header("User-Agent",
                                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"))
                 .andExpect(status().isOk())
