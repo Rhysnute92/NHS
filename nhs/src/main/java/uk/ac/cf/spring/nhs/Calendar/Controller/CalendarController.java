bd14914514de47a778d9c4018272f5bc7825672c
Rhys Nute <nuterd@cardiff.ac.uk>
2024-07-30 13:59:15 +0100

    updated

--- nhs/src/main/java/uk/ac/cf/spring/nhs/Calendar/Controller/CalendarController.java
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/Calendar/Controller/CalendarController.java
@@ -1,6 +1,7 @@
 package uk.ac.cf.spring.nhs.Calendar.Controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,15 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-import uk.ac.cf.spring.nhs.Calendar.Repositories.Calendar;
 import uk.ac.cf.spring.nhs.Calendar.Repositories.CalendarRepository;
 import uk.ac.cf.spring.nhs.Common.util.DeviceDetector;
 
+import java.util.Calendar;
 import java.util.List;
 
 @Controller
 public class CalendarController {
 
+    @Autowired
+    private CalendarRepository calendarRepository;
     @GetMapping("/calendar")
     public ModelAndView Calendar(HttpServletRequest request) {
         ModelAndView modelAndView = new ModelAndView();
@@ -34,5 +37,10 @@ public class CalendarController {
     public ModelAndView getMobileAddAppt(){
         ModelAndView modelAndView = new ModelAndView("mobile/Calendar/addappointment");
         return modelAndView;
+    }
+
+    @PostMapping
+    public ModelAndView appointmentAddedToCalendar(@Valid @ModelAttribute("appt_sent")Calendar calendar, BindingResult bindingResult, Model model) {
+        ModelAndView modelAndView = new ModelAndView("addappointment")
     }
 }
