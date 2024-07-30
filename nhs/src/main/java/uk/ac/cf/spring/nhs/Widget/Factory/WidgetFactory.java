7d7197c9a45bd045cfa9f1638635acc53a23c3ce
Patrick <longpt@cardiff.ac.uk>
2024-07-30 11:03:35 +0100

    added factory -Patrick

--- nhs/src/main/java/uk/ac/cf/spring/nhs/Widget/Factory/WidgetFactory.java
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/Widget/Factory/WidgetFactory.java
@@ -1,5 +1,16 @@
 package uk.ac.cf.spring.nhs.Widget.Factory;
 
+import uk.ac.cf.spring.nhs.Widget.Model.TaskCompletionWidget;
+import uk.ac.cf.spring.nhs.Widget.Model.Widget;
 
 public class WidgetFactory {
+
+    public static Widget getWidget(String widgetType) {
+        if ("task-completion".equalsIgnoreCase(widgetType)) {
+            return new TaskCompletionWidget();
+        }
+        // Add further widget types here
+        
+        return null;
+    }
 }
