260c9e34b6ad36b5680d475ec7a6877617381689
Rhys Nute <nuterd@cardiff.ac.uk>
2024-07-31 16:06:21 +0100

    added sidebar nav

--- nhs/src/main/java/uk/ac/cf/spring/nhs/Calendar/Controller/CalendarController.java
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/Calendar/Controller/CalendarController.java
@@ -3,8 +3,12 @@ package uk.ac.cf.spring.nhs.Calendar.Controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 import uk.ac.cf.spring.nhs.Common.util.DeviceDetector;
+import uk.ac.cf.spring.nhs.Common.util.NavMenuItem;
+
+import java.util.List;
 
 @Controller
 public class CalendarController {
@@ -25,4 +29,13 @@ public class CalendarController {
         ModelAndView modelAndView = new ModelAndView("mobile/Calendar/addappointment");
         return modelAndView;
     }
+
+    @ModelAttribute("navMenuItems")
+    public List<NavMenuItem> navMenuItems() {
+        return List.of(
+                new NavMenuItem("Schedule", "/calendar", "fa-regular fa-calendar-days"),
+                new NavMenuItem("Add Appointment", "/mobileaddappt", "fa-solid fa-calendar-check")
+        );
+    }
+
 }
