dda6589d466c4b7da7d1977381137420ff4cb6a5
Rowan Burnett <burnettrs@cardiff.ac.uk>
2024-08-03 11:06:04 +0100

    Use file storage service to store uploaded photos

--- nhs/src/main/java/uk/ac/cf/spring/nhs/Diary/Service/DiaryEntryService.java
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/Diary/Service/DiaryEntryService.java
@@ -7,6 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 import uk.ac.cf.spring.nhs.Diary.DTO.CheckinForm;
 import uk.ac.cf.spring.nhs.Diary.Entity.*;
 import uk.ac.cf.spring.nhs.Diary.Repository.*;
+import uk.ac.cf.spring.nhs.Common.util.FileStorageService;
 
 import java.util.*;
 
@@ -16,14 +17,17 @@ public class DiaryEntryService {
     private final DiaryEntryRepository diaryEntryRepository;
     private final PhotoRepository photoRepository;
     private final SymptomRepository symptomRepository;
+    private final FileStorageService fileStorageService;
 
     @Autowired
     public DiaryEntryService(DiaryEntryRepository diaryEntryRepository,
                              PhotoRepository photoRepository,
-                             SymptomRepository symptomRepository) {
+                             SymptomRepository symptomRepository,
+                             FileStorageService fileStorageService) {
         this.diaryEntryRepository = diaryEntryRepository;
         this.photoRepository = photoRepository;
         this.symptomRepository = symptomRepository;
+        this.fileStorageService = fileStorageService;
     }
 
     @Transactional
@@ -54,20 +58,22 @@ public class DiaryEntryService {
     @Transactional
     public void createAndSaveDiaryEntry(CheckinForm checkinForm, List<MultipartFile> photos) throws Exception {
 
-        int userId = 1; // Hardcoded user ID for testing
+        int userId = 1;
 
         DiaryEntry diaryEntry = new DiaryEntry(userId, new Date());
 
+        // Mood
         if (checkinForm.getMood() != null && !checkinForm.getMood().isEmpty()) {
             diaryEntry.setMood(DiaryMood.valueOf(checkinForm.getMood()));
         }
 
         diaryEntry.setNotes(checkinForm.getNotes());
 
+        // Photos
         if (photos != null && !photos.isEmpty()) {
             Set<DiaryPhoto> diaryPhotos = new HashSet<>();
             for (MultipartFile photo : photos) {
-                String photoUrl = savePhoto(photo);
+                String photoUrl = fileStorageService.storeFile(photo);
                 Photo photoEntity = new Photo(photoUrl, new Date(), "", userId);
                 photoRepository.save(photoEntity);
                 DiaryPhoto diaryPhoto = new DiaryPhoto();
@@ -78,6 +84,7 @@ public class DiaryEntryService {
             diaryEntry.setPhotos(diaryPhotos);
         }
 
+        // Symptoms
         Set<DiarySymptom> diarySymptoms = new HashSet<>();
         if (checkinForm.getTroubleSleepingSeverity() != null) {
             Symptom symptom = new Symptom("Trouble sleeping", checkinForm.getTroubleSleepingSeverity(), new Date(), true, userId);
@@ -106,10 +113,5 @@ public class DiaryEntryService {
         diaryEntry.setSymptoms(diarySymptoms);
 
         diaryEntryRepository.save(diaryEntry);
-    }
-
-    private String savePhoto(MultipartFile photo) {
-        // Placeholder for now
-        return "photo-url";
     }
 }
