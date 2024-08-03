36ffd5cb6ce80ac232d62e8f35cdb8de6d42ead0
Rowan Burnett <burnettrs@cardiff.ac.uk>
2024-08-03 10:33:53 +0100

    Add controller post mapping to submit check-in form

--- nhs/src/main/java/uk/ac/cf/spring/nhs/Diary/Controller/DiaryController.java
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/Diary/Controller/DiaryController.java
@@ -2,17 +2,20 @@ package uk.ac.cf.spring.nhs.Diary.Controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import uk.ac.cf.spring.nhs.Common.util.DeviceDetector;
 import uk.ac.cf.spring.nhs.Common.util.NavMenuItem;
+import uk.ac.cf.spring.nhs.Diary.DTO.CheckinForm;
 import uk.ac.cf.spring.nhs.Diary.Entity.DiaryEntry;
 import uk.ac.cf.spring.nhs.Diary.Service.DiaryEntryService;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,7 +28,7 @@ public class DiaryController {
 
     @GetMapping("")
     public String diary(Model model) {
-        List<DiaryEntry> diaryEntries = diaryService.getDiaryEntries();
+        List<DiaryEntry> diaryEntries = diaryEntryService.getAllDiaryEntries();
         model.addAttribute("diaryEntries", diaryEntries);
         return "diary/diary";
     }
@@ -33,6 +36,25 @@ public class DiaryController {
     @GetMapping("/checkin")
     public String checkin(Model model) {
         return "diary/checkin";
+    }
+
+
+    @PostMapping("/checkin")
+    public ResponseEntity<Map<String, String>> checkin(
+            @ModelAttribute CheckinForm checkinForm,
+            @RequestParam(value = "checkin-photos-upload", required = false) List<MultipartFile> photos
+    ) {
+        Map<String, String> response = new HashMap<>();
+        try {
+            diaryEntryService.createAndSaveDiaryEntry(checkinForm, photos);
+
+            response.put("message", "Check-in successful!");
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            response.put("message", "Check-in failed!");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
     }
 
     @ModelAttribute("navMenuItems")
