eed6be190fa91bd6f7a3a71f871acf9a2dd28d06
Rowan Burnett <burnettrs@cardiff.ac.uk>
2024-08-03 10:48:30 +0100

    Save submitted check-in data in database

--- nhs/src/main/java/uk/ac/cf/spring/nhs/Diary/Service/DiaryEntryService.java
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/Diary/Service/DiaryEntryService.java
@@ -3,20 +3,27 @@ package uk.ac.cf.spring.nhs.Diary.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import uk.ac.cf.spring.nhs.Diary.Entity.DiaryEntry;
-import uk.ac.cf.spring.nhs.Diary.Repository.DiaryEntryRepository;
+import org.springframework.web.multipart.MultipartFile;
+import uk.ac.cf.spring.nhs.Diary.DTO.CheckinForm;
+import uk.ac.cf.spring.nhs.Diary.Entity.*;
+import uk.ac.cf.spring.nhs.Diary.Repository.*;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class DiaryEntryService {
 
     private final DiaryEntryRepository diaryEntryRepository;
+    private final PhotoRepository photoRepository;
+    private final SymptomRepository symptomRepository;
 
     @Autowired
-    public DiaryEntryService(DiaryEntryRepository diaryEntryRepository) {
+    public DiaryEntryService(DiaryEntryRepository diaryEntryRepository,
+                             PhotoRepository photoRepository,
+                             SymptomRepository symptomRepository) {
         this.diaryEntryRepository = diaryEntryRepository;
+        this.photoRepository = photoRepository;
+        this.symptomRepository = symptomRepository;
     }
 
     @Transactional
@@ -42,5 +49,67 @@ public class DiaryEntryService {
     @Transactional
     public void deleteDiaryEntryById(int id) {
         diaryEntryRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void createAndSaveDiaryEntry(CheckinForm checkinForm, List<MultipartFile> photos) throws Exception {
+
+        int userId = 1; // Hardcoded user ID for testing
+
+        DiaryEntry diaryEntry = new DiaryEntry(userId, new Date());
+
+        if (checkinForm.getMood() != null && !checkinForm.getMood().isEmpty()) {
+            diaryEntry.setMood(DiaryMood.valueOf(checkinForm.getMood()));
+        }
+
+        diaryEntry.setNotes(checkinForm.getNotes());
+
+        if (photos != null && !photos.isEmpty()) {
+            Set<DiaryPhoto> diaryPhotos = new HashSet<>();
+            for (MultipartFile photo : photos) {
+                String photoUrl = savePhoto(photo);
+                Photo photoEntity = new Photo(photoUrl, new Date(), "", userId);
+                photoRepository.save(photoEntity);
+                DiaryPhoto diaryPhoto = new DiaryPhoto();
+                diaryPhoto.setPhoto(photoEntity);
+                diaryPhoto.setDiaryEntry(diaryEntry);
+                diaryPhotos.add(diaryPhoto);
+            }
+            diaryEntry.setPhotos(diaryPhotos);
+        }
+
+        Set<DiarySymptom> diarySymptoms = new HashSet<>();
+        if (checkinForm.getTroubleSleepingSeverity() != null) {
+            Symptom symptom = new Symptom("Trouble sleeping", checkinForm.getTroubleSleepingSeverity(), new Date(), true, userId);
+            symptomRepository.save(symptom);
+            DiarySymptom diarySymptom = new DiarySymptom();
+            diarySymptom.setSymptom(symptom);
+            diarySymptom.setDiaryEntry(diaryEntry);
+            diarySymptoms.add(diarySymptom);
+        }
+        if (checkinForm.getPainSeverity() != null) {
+            Symptom symptom = new Symptom("Pain", checkinForm.getPainSeverity(), new Date(), true, userId);
+            symptomRepository.save(symptom);
+            DiarySymptom diarySymptom = new DiarySymptom();
+            diarySymptom.setSymptom(symptom);
+            diarySymptom.setDiaryEntry(diaryEntry);
+            diarySymptoms.add(diarySymptom);
+        }
+        if (checkinForm.getNumbnessSeverity() != null) {
+            Symptom symptom = new Symptom("Numbness", checkinForm.getNumbnessSeverity(), new Date(), true, userId);
+            symptomRepository.save(symptom);
+            DiarySymptom diarySymptom = new DiarySymptom();
+            diarySymptom.setSymptom(symptom);
+            diarySymptom.setDiaryEntry(diaryEntry);
+            diarySymptoms.add(diarySymptom);
+        }
+        diaryEntry.setSymptoms(diarySymptoms);
+
+        diaryEntryRepository.save(diaryEntry);
+    }
+
+    private String savePhoto(MultipartFile photo) {
+        // Placeholder for now
+        return "photo-url";
     }
 }
