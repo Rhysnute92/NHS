0e6a7cd5ea2138d99fbd6d4f80cbe4bdc128df55
Rhys Nute <nuterd@cardiff.ac.uk>
2024-08-03 11:29:36 +0100

    updates

--- nhs/src/main/java/uk/ac/cf/spring/nhs/Calendar/Controller/CalendarController.java
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/Calendar/Controller/CalendarController.java
@@ -1,46 +1,46 @@
 package uk.ac.cf.spring.nhs.Calendar.Controller;
 
-import java.util.List;
-
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import uk.ac.cf.spring.nhs.Appointments.Model.Appointment;
-import uk.ac.cf.spring.nhs.Appointments.Service.AppointmentService;
+import org.springframework.web.servlet.ModelAndView;
 import uk.ac.cf.spring.nhs.Common.util.DeviceDetector;
 import uk.ac.cf.spring.nhs.Common.util.NavMenuItem;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/calendar")
 public class CalendarController {
-
-    @Autowired
-    private AppointmentService appointmentService;
-
-    @GetMapping
-    public String calendar(HttpServletRequest request, Model model) {
+    @GetMapping("/calendar")
+    public ModelAndView Calendar(HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView();
         if (DeviceDetector.isMobile(request)) {
-            return "calendar/mobile/calendar";
+            modelAndView.setViewName("calendar/mobile/calendar");
         } else {
-            return "calendar/desktop/calendar";
+            modelAndView.setViewName("calendar/desktop/calendar");
+
         }
+        return modelAndView;
     }
 
-    @GetMapping("/mobileaddappt")
-    public String addAppointment(Model model) {
-        model.addAttribute("appointment", new Appointment());
-        return "calendar/mobile/addappointment";
+    @GetMapping("/addappointment")
+    public ModelAndView getMobileAddAppt(HttpServletRequest request){
+        ModelAndView modelAndView = new ModelAndView();
+        if (DeviceDetector.isMobile(request)){
+            modelAndView.setViewName("calendar/mobile/addappointment");
+        } else {
+            modelAndView.setViewName("calendar/desktop/addappointment");
+        }
+        return modelAndView;
     }
 
     @ModelAttribute("navMenuItems")
     public List<NavMenuItem> navMenuItems() {
         return List.of(
                 new NavMenuItem("Schedule", "/calendar", "fa-regular fa-calendar-days"),
-                new NavMenuItem("Add Appointment", "/calendar/mobileaddappt", "fa-solid fa-calendar-check"));
+                new NavMenuItem("Add Appointment", "/mobileaddappt", "fa-solid fa-calendar-check")
+        );
     }
+
 }
