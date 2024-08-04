60105a2302dfb3c0ce6b397c74c52be7a148b443
Patrick <longpt@cardiff.ac.uk>
2024-08-04 18:54:12 +0100

    pulled from dev

--- nhs/src/main/java/uk/ac/cf/spring/nhs/Calendar/Repositories/Calendar.java
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/Calendar/Repositories/Calendar.java
@@ -1,10 +1,6 @@
 package uk.ac.cf.spring.nhs.Calendar.Repositories;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
