e17639aae7e745a1059c1dd2b9e8b88e3246dc83
c1950120 <murinsk@cardiff.ac.uk>
2024-08-05 06:21:23 +0100

    update comment

--- nhs/src/main/java/uk/ac/cf/spring/nhs/Security/AuthenticationInterface.java
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/Security/AuthenticationInterface.java
@@ -3,9 +3,8 @@ package uk.ac.cf.spring.nhs.Security;
 import org.springframework.security.core.Authentication;
 
 //Interface for accessing the current session user
-//To use need to @Autowire it in the class and create and Authentication object
-//by calling the getAuthentication() function.
-//After that the User Details functions can be used on the object to retrieve specific data.
+//To use need to @Autowire it in the desired class
+//Example use can be seen in AccountController 
 public interface AuthenticationInterface {
     Authentication getAuthentication();
 }
