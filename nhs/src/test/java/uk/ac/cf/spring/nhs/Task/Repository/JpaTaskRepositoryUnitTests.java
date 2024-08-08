4d97e946759b13e2b30f199da115773af4b4bcf4
Patrick <longpt@cardiff.ac.uk>
2024-08-08 14:26:21 +0100

    reconfigured build.gradle to be more readable

--- nhs/src/test/java/uk/ac/cf/spring/nhs/Task/Repository/JpaTaskRepositoryUnitTests.java
+++ nhs/src/test/java/uk/ac/cf/spring/nhs/Task/Repository/JpaTaskRepositoryUnitTests.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
