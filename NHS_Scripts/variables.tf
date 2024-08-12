af9b6370d0babbf1d899e5f0784286de693458db
c1950120 <murinsk@cardiff.ac.uk>
2024-08-12 07:27:07 +0100

    add terraform scripts

---
+++ NHS_Scripts/variables.tf
@@ -0,0 +1,11 @@
+variable "flavor" { default = "m1.medium" }
+variable "image" { default = "Rocky Linux 9 20220830" }
+variable "name" { default = "NHS_Dev_Live" }
+
+variable "network" { default = "default" }
+variable "keypair" { default = "nhs_proj_keypair" }
+
+variable "pool" { default = "cscloud_private_floating" }
+variable "server_script" { default = ".nhs_dev.sh" }
+variable "security_description" { default = "NHS dev server security" }
+variable "security_name" { default = "nhs_dev_security" }
