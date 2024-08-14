10f321a45575434675e9dc5d3d933853f20d3c60
c1950120 <murinsk@cardiff.ac.uk>
2024-08-14 08:44:04 +0100

    treatment page for legs

--- nhs/src/main/resources/static/js/education/treatmentSpec.js
+++ nhs/src/main/resources/static/js/education/treatmentSpec.js
@@ -11,6 +11,19 @@ for (var i = 0; i < coll.length; i++) {
   });
 }
 
+var coll_vid = document.getElementsByClassName("collapsible_video_button");
+for (var i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 let gallery = document.getElementById("display_item").getElementsByTagName("img");
 function backClick(){
   for (var i = 0; i < gallery.length; i++){
