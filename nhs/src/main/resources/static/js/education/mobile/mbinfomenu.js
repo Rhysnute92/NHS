dd4f4d043ea3486684d2925285f56da03848dc2f
c1950120 <murinsk@cardiff.ac.uk>
2024-08-14 10:45:16 +0100

    mobile treatment spec css

--- nhs/src/main/resources/static/js/education/mobile/mbinfomenu.js
+++
@@ -1,7 +0,0 @@
-function menuChange() {
-  if (document.getElementById("mbim").style.width == "0px" || document.getElementById("mbim").style.width == 0){
-    document.getElementById("mbim").style.width = "70vw"
-  } else {
-    document.getElementById("mbim").style.width = "0";
-  }
-}
