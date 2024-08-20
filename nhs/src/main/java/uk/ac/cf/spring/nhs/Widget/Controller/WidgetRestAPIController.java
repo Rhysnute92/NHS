92608433e1cae382d5f2ad14dc22fa817d54ba00
Rhys Nute <nuterd@cardiff.ac.uk>
2024-08-20 15:34:46 +0100

    fixes

--- nhs/src/main/java/uk/ac/cf/spring/nhs/Widget/Controller/WidgetRestAPIController.java
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/Widget/Controller/WidgetRestAPIController.java
@@ -1,13 +1,16 @@
 package uk.ac.cf.spring.nhs.Widget.Controller;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import uk.ac.cf.spring.nhs.Appointments.DTO.AppointmentDTO;
 import uk.ac.cf.spring.nhs.Security.AuthenticationInterface;
+import uk.ac.cf.spring.nhs.Appointments.Service.AppointmentService;
 import uk.ac.cf.spring.nhs.Security.CustomUserDetails;
 import uk.ac.cf.spring.nhs.UserTask.DTO.UserTaskDTO;
 import uk.ac.cf.spring.nhs.UserTask.Service.UserTaskService;
@@ -50,5 +53,23 @@ public class WidgetRestAPIController {
 
         return new UserTaskDTO(totalTasks, completedTasks);
     }
-
+//    @GetMapping("/api/widgets/appointments-tracker/data")
+//    public List<AppointmentDTO> getAppointmentsTrackerData(@RequestParam(name = "date", required = false) LocalDate date) {
+//
+//        Object principal = authenticationFacade.getAuthentication().getPrincipal();
+//        Long userId = ((CustomUserDetails) principal).getUserId();
+//
+//        if (date == null) {
+//            date = LocalDate.now();
+//            System.out.println("Defaulted to current date: " + date);
+//        } else {
+//            System.out.println("Requested date: " + date);
+//        }
+//
+//        List<AppointmentDTO> appointments = appointmentService.getAppointmentsForDate(userId, date);
+//
+//        System.out.println("Appointments for user " + userId + " on date " + date + ": " + appointments.size());
+//
+//        return appointments;
+//    }
 }
