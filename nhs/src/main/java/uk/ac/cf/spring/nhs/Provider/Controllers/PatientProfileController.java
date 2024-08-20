82ed25264481946883db5806bffc153805a37da7
c1950120 <murinsk@cardiff.ac.uk>
2024-08-20 21:03:18 +0100

    added userID session attribute to patient view

--- nhs/src/main/java/uk/ac/cf/spring/nhs/Provider/Controllers/PatientProfileController.java
+++ nhs/src/main/java/uk/ac/cf/spring/nhs/Provider/Controllers/PatientProfileController.java
@@ -1,18 +1,25 @@
 package uk.ac.cf.spring.nhs.Provider.Controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import uk.ac.cf.spring.nhs.AddPatient.Service.PatientService;
 import uk.ac.cf.spring.nhs.Common.util.NavMenuItem;
 
 
 @Controller
+@SessionAttributes("userID")
 @RequestMapping("/patientprofile")
 public class PatientProfileController {
 
@@ -32,6 +39,19 @@ public class PatientProfileController {
             new NavMenuItem("Email history", " ", "fa-solid fa-book")
         );
     }
+
+    @ModelAttribute("userID")
+    public Long profileUser(){
+        Long userId = 0L;
+        return userId;
+    }
+
+    @GetMapping("/{id}")
+    public String openProfile(@PathVariable("id") int id, Map<String, Object> model) {
+        Long userID = Long.valueOf(id);
+        model.put("userID", userID);
+        return "redirect:/patientprofile/info";
+    } 
 
     @GetMapping("/info")
     public String patientProfileAdmin() {return "patientprofile/profileInfo";}
